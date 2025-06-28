d06b6afb2fe37669c3189e61a83e7b23b55a6a95
Subhashis <subhashisdhara123@gmail.com>
2025-06-28 15:13:43 +0530

    style: format code for consistency and readability

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
+import express from "express";
 import "dotenv/config";
-import cors from 'cors';
-import http from 'http';
-import { connectDB } from './lib/db.js';
-import userRouter from './routes/userRoutes.js';
-import messageRouter from './routes/messageRoutes.js';
-import { Server } from 'socket.io';
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 // Create Express app and HTTP server
 const app = express();
@@ -13,43 +13,41 @@ const server = http.createServer(app);
 
 // Initialize Socket.IO
 export const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
-})
+  cors: {
+    origin: "*",
+  },
+});
 
 // Store online users
 export const userSocketMap = {}; // { userId: socketId }
 
 // Socket.IO connection handling
 io.on("connection", (socket) => {
-    const userId = socket.handshake.query.userId;
-    console.log(`User connected: ${userId}`);
+  const userId = socket.handshake.query.userId;
+  console.log(`User connected: ${userId}`);
 
-    if(userId) {
-        userSocketMap[userId] = socket.id;
-    }
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
 
-    // Emit online users to all clients
+  // Emit online users to all clients
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Handle user disconnection
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${userId}`);
+    delete userSocketMap[userId];
+
+    // Emit updated online users
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    // Handle user disconnection
-    socket.on("disconnect", () => {
-        console.log(`User disconnected: ${userId}`);
-        delete userSocketMap[userId];
-
-        // Emit updated online users
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
+  });
 });
 
-
-
 // Middleware setup
-app.use(express.json({limit: '4mb'}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
-app.use("/api/status", (req, res)=>res.send("Server is running"));
+app.use("/api/status", (req, res) => res.send("Server is running"));
 // Import routes
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
@@ -58,7 +56,11 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 // Start the server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+  });
+}
+
+export default server;
