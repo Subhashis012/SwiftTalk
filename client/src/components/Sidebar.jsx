a3c455cb9eb095ae3348e5e490f9200ed503b786
Subhashis <subhashisdhara123@gmail.com>
2025-06-29 10:15:08 +0530

    refactor: clean up Sidebar component by removing unnecessary console log and simplifying className logic

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -24,10 +24,7 @@ const Sidebar = () => {
     ? users.filter((user) =>
         user.fullName.toLowerCase().includes(input.toLowerCase())
       )
-    : users;
-
-    console.log(users);
-    
+    : users; 
     
 
   useEffect(() => {
@@ -36,9 +33,7 @@ const Sidebar = () => {
 
   return (
     <div
-      className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
-        selectedUser ? "max-md:hidden" : ""
-      }`}
+      className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white `}
     >
       <div className="pb-5">
         <div className="flex justify-between items-center">
